510677f4dd1212ab262535746082a5746c41cfec
suankidev <suankisingh@outlook.com>
2023-05-31 23:30:19 +0530

    testing

--- src/init.py
+++ src/init.py
@@ -22,23 +22,5 @@ if os.path.exists('python.zip'):
 else:
     sys.path.insert(0, './python')
 
-
-def init_pipeline(spark: pyspark.sql.session.SparkSession):
-    if os.path.exists(setenv.base_path):
-        for file in os.listdir(setenv.base_path):
-            print(file)
-
-
-def build_output(entry: pathlib.Path, long=False):
-    if long:
-        size = entry.stat().st_size
-        date = datetime.datetime.fromtimestamp((entry.stat().st_mtime)).strftime("%b %d %H:%M:%S")
-
-        return f"{size:>6d} {date} {entry.name}"
-    return entry.name
-
-
 if __name__ == '__main__':
     ingest.ingest_data()
-
-
