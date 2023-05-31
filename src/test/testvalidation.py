16d72c185c393065944c84c3224015edf1ee0e79
suankidev <suankisingh@outlook.com>
2023-06-01 00:12:25 +0530

    testing

--- src/test/testvalidation.py
+++ src/test/testvalidation.py
@@ -3,15 +3,14 @@ import unittest
 import logging
 import logging.config
 
-logging.config.fileConfig("../conf/logging.conf")
 
-logger = logging.getLogger(__name__)
-
-
-logger.info("starting validatio...! ")
+# logging.config.fileConfig("../conf/logging.conf")
+#
+# logger = logging.getLogger(__name__)
+#
+#
+# logger.info("starting validatio...! ")
 
 class ValidationTest(unittest.TestCase):
     def test_one(self):
-        logger.info("checking values 2")
         self.assertEqual(10, 10)
-
